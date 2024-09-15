788845e031682ac3a95576dff2d05c24b6a79852
Talha <talhahaider074@gmail.com>
2024-09-15 23:27:02 +0500

    url / rempvbed

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let dev = {
     baseUrl :"http://localhost:5000/api/v1"
 }
 let production = {
-    baseUrl :"https://music-plugged-backend-wrox255du-talha2224s-projects.vercel.app/"
+    baseUrl :"https://music-plugged-backend-wrox255du-talha2224s-projects.vercel.app"
 }
 
 
