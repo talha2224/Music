1251b84e4abb01dacb37e59253642ffe1cbf5422
Talha <talhahaider074@gmail.com>
2024-09-27 21:44:34 +0500

    baseUrl changed

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let dev = {
     baseUrl :"http://localhost:5000/api/v1"
 }
 let production = {
-    baseUrl :"https://music-plugged-backend-t96g-ods2l2e3e-talha2224s-projects.vercel.app"
+    baseUrl :"https://music-plugged-backend-t96g.vercel.app/api/v1"
 }
 
 
