61c244e26061b05531132773f2af1ad5a9ea0cbb
Talha Haider <talhahaider074@gmail.com>
2024-10-09 21:47:58 +0500

    production url changed

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ let dev = {
     stripeProductKey: "price_1Q4GusASyMRcymO6kkqdreUp"
 }
 let production = {
-    baseUrl: "https://music-plugged-backend-t96g.vercel.app/api/v1",
+    baseUrl: "https://hammerhead-app-5gr64.ondigitalocean.app/api/v1",
     stripePublishKey: "pk_test_51OjJpTASyMRcymO6x2PBK1nrHChycNMNXj7HHvTRffOp5xufCj3WRSCLoep1tGp5Ilx3IWj7ck5yqrwEH8VSRKn80055Kvyelu",
     stripeSecretKey: "sk_test_51OjJpTASyMRcymO6FVBewDoB2x4Wi5tq5uX5PYSfkAC2pU0sZvWJbZIqGoMTnzEYYFjFh4jbcWYD3OyFc761otRt00tX4j1UO2",
     stripeProductKey: "price_1Q4GusASyMRcymO6kkqdreUp"
